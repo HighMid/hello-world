5f720d4e9a355b15789bb5f788cd9080c7b3cb02
HighMid <xstar671@gmail.com>
2023-10-11 20:08:12 -0400

    Updated with Javascript, practice for lab06

---
+++ yes.js
@@ -0,0 +1,10 @@
+'use strict';
+
+alert('Sorry for annoying you. . .');
+
+let user = prompt('But who are you?!');
+if(user == null){
+    user = 'Batman';
+}
+alert('Oh nice to meet you ' + user);
+console.log('What the nugget are you looking over here for!' + user);
